4b7ac8a12f37f518e23d423d62520be4e33909f5
grimlink <sean.grimlink@gmail.com>
2022-11-24 23:42:30 +0100

    ADD: new tests for Vue

---
+++ _tests_/tw.vue
@@ -0,0 +1,17 @@
+<script setup>
+console.log("Hello World!");
+</script>
+
+<template>
+    <p class="special-w-class">Hello there</p>
+</template>
+
+<style>
+.special-w-class {
+    @apply bg-purple-400;
+
+    @screen md {
+        @apply bg-purple-500;
+    }
+}
+</style>
